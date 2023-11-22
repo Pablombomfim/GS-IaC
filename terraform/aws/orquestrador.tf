a5a9eac5a14b558f6f4b936ca202b1258d8b6da4
Pablombomfim <pablo.mb3107@gmail.com>
2023-11-22 20:52:50 -0300

    Alteração do caminho orquestrador

--- terraform/aws/orquestrador.tf
+++ terraform/aws/orquestrador.tf
@@ -4,6 +4,6 @@ module "rede" {
 
 module "compute" {
     source = "./modules/compute"
-    vpc_id = module.rede.vpc_id
-    subnet_id = module.rede.subnet_id
+    vpc_id = modules.rede.vpc_id
+    subnet_id = modules.rede.subnet_id
 }
