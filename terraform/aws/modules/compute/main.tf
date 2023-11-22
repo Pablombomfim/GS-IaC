ecb002ad1ca8d8b70690e3c4edb36b3fa526111f
Pablombomfim <pablo.mb3107@gmail.com>
2023-11-22 20:38:59 -0300

    Tentativa correção vpc ID

--- terraform/aws/modules/compute/main.tf
+++ terraform/aws/modules/compute/main.tf
@@ -1,17 +1,15 @@
-# RESOURCE: SECURITY GROUP
-
 resource "aws_security_group" "sg" {
   name        = "Sgec2"
   description = "Allow SSH and HTTP inbound traffic"
-  vpc_id      = var.vpc_id
+  vpc_id      = var.vpc_id  # Certifique-se de que este é o ID da VPC correta
 
-    ingress {
+  ingress {
     description = "All the ports"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/16"]
-    }
+  }
 
   egress {
     description = "Allow all outbound traffic"
@@ -23,18 +21,18 @@ resource "aws_security_group" "sg" {
 }
 
 resource "aws_instance" "web" {
-    count                  = 2
-    ami                    = "ami-0230bd60aa48260c6"
-    subnet_id              = var.subnet_id
-    instance_type          = "t2.micro"
-    user_data              = file("./modules/compute/init/instance.sh")
-    vpc_security_group_ids = [aws_security_group.sg.id]
-    associate_public_ip_address = true
+  count                  = 2
+  ami                    = "ami-0230bd60aa48260c6"
+  subnet_id              = var.subnet_id
+  instance_type          = "t2.micro"
+  user_data              = file("./modules/compute/init/instance.sh")
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  associate_public_ip_address = true
 }
 
 resource "aws_elb" "load_balancer" {
   name               = "loadbalancer"
-  security_groups    = [aws_security_group.sg.id]
+  security_groups    = [aws_security_group.sg.id]  # Use o ID do grupo de segurança criado
   availability_zones = ["us-east-1a"]
 
   listener {
@@ -46,8 +44,3 @@ resource "aws_elb" "load_balancer" {
 
   instances = aws_instance.web[*].id
 }
-
-
-
-
-
