1aa8cda5304c0555cf6230ec8f5f00b993a56f3f
Pablombomfim <pablo.mb3107@gmail.com>
2023-11-22 20:33:41 -0300

    Habilitar o ipv4

--- terraform/aws/modules/compute/main.tf
+++ terraform/aws/modules/compute/main.tf
@@ -5,20 +5,6 @@ resource "aws_security_group" "sg" {
   description = "Allow SSH and HTTP inbound traffic"
   vpc_id      = var.vpc_id
 
-  ingress {
-    description = "SSH from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/16"]
-  }
-  ingress {
-    description = "HTTP from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/16"]
-  }
     ingress {
     description = "All the ports"
     from_port   = 0
@@ -37,12 +23,13 @@ resource "aws_security_group" "sg" {
 }
 
 resource "aws_instance" "web" {
-  count                  = 2
-  ami                    = "ami-0230bd60aa48260c6"
-  subnet_id              = var.subnet_id
-  instance_type          = "t2.micro"
-  user_data              = file("./modules/compute/init/instance.sh")
-  vpc_security_group_ids = [aws_security_group.sg.id]
+    count                  = 2
+    ami                    = "ami-0230bd60aa48260c6"
+    subnet_id              = var.subnet_id
+    instance_type          = "t2.micro"
+    user_data              = file("./modules/compute/init/instance.sh")
+    vpc_security_group_ids = [aws_security_group.sg.id]
+    associate_public_ip_address = true
 }
 
 resource "aws_elb" "load_balancer" {
