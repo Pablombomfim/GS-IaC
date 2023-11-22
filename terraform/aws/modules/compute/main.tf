9c168488ff89be619b1b4e83fe38f3bc93b36140
Pablombomfim <pablo.mb3107@gmail.com>
2023-11-22 20:01:29 -0300

    Novo valor de imagem

--- terraform/aws/modules/compute/main.tf
+++ terraform/aws/modules/compute/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
     count = 2
-    ami           = "ami-0c94855ba95c574c8"
+    ami           = "ami-0230bd60aa48260c6"
     instance_type = "t2.micro"
     key_name      = "my-key"
     user_data     = file("./modules/compute/init/instance.sh") 
