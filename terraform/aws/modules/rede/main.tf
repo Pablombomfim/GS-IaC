c13e18e533cf9b9d6cb828a46b23f5df0fbbb385
Pablombomfim <pablo.mb3107@gmail.com>
2023-11-22 21:17:30 -0300

    teste

--- terraform/aws/modules/rede/main.tf
+++ terraform/aws/modules/rede/main.tf
@@ -52,27 +52,20 @@ resource "aws_security_group" "sg2" {
 
 
 resource "aws_instance" "web" {
-  count                  = 2
-  ami                    = "ami-0230bd60aa48260c6"
-  subnet_id              = aws_subnet.subec2.id
-  instance_type          = "t2.micro"
-  user_data              = file("./modules/compute/init/instance.sh")
-  vpc_security_group_ids = [aws_security_group.sg2.id]
+  count                       = 2
+  ami                         = "ami-0230bd60aa48260c6"
+  subnet_id                   = aws_subnet.subec2.id
+  instance_type               = "t2.micro"
+  user_data                   = file("./modules/compute/init/instance.sh")
+  vpc_security_group_ids      = [aws_security_group.sg2.id]
   associate_public_ip_address = true
 }
 
 
-resource "aws_elb" "load_balancer" {
-  name               = "loadbalancer"
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
   security_groups    = [aws_security_group.sg2.id]
-  availability_zones = ["us-east-1a"]
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-
-  instances = [aws_instance.web[0].id, aws_instance.web[1].id]
+  subnets            = [aws_subnet.subec2.id]
 }
