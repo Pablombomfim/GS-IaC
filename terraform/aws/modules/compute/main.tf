99d797fee9e0c59b0b25667900f482fac12aba2e
Pablombomfim <pablo.mb3107@gmail.com>
2023-11-22 21:09:08 -0300

    Chumbado

--- terraform/aws/modules/compute/main.tf
+++ terraform/aws/modules/compute/main.tf
@@ -1,11 +1,2 @@
 
-resource "aws_instance" "web" {
-  count                  = 2
-  ami                    = "ami-0230bd60aa48260c6"
-  subnet_id              = var.subnet_id
-  instance_type          = "t2.micro"
-  user_data              = file("./modules/compute/init/instance.sh")
-  vpc_security_group_ids = [var.id-sg]
-  associate_public_ip_address = true
-}
 
--- terraform/aws/modules/rede/main.tf
+++ terraform/aws/modules/rede/main.tf
@@ -50,6 +50,18 @@ resource "aws_security_group" "sg2" {
 }
 
 
+
+resource "aws_instance" "web" {
+  count                  = 2
+  ami                    = "ami-0230bd60aa48260c6"
+  subnet_id              = aws_subnet.subec2.id
+  instance_type          = "t2.micro"
+  user_data              = file("./modules/compute/init/instance.sh")
+  vpc_security_group_ids = [aws_security_group.sg2.id]
+  associate_public_ip_address = true
+}
+
+
 resource "aws_elb" "load_balancer" {
   name               = "loadbalancer"
   security_groups    = [aws_security_group.sg2.id]
