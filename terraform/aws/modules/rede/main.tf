8869fdea564550daeaafda2c3403827586faceef
Pablombomfim <pablo.mb3107@gmail.com>
2023-11-22 21:22:02 -0300

    teste LB

--- terraform/aws/modules/rede/main.tf
+++ terraform/aws/modules/rede/main.tf
@@ -68,10 +68,27 @@ resource "aws_instance" "web" {
 }
 
 
-resource "aws_lb" "test" {
-  name               = "test-lb-tf"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.sg2.id]
-  subnets            = [aws_subnet.subec2.id, aws_subnet.subec2-2.id]
+resource "aws_lb_target_group" "target_group" {
+  name     = "target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "attachment" {
+  count            = 2
+  target_group_arn = aws_lb_target_group.target_group.arn
+  target_id        = aws_instance.web[count.index].id
+  port             = 80
 }
