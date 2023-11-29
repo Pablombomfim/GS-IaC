ce6873a7f11e1b9106ff317133db2d8095b15620
Pablombomfim <pablo.mb3107@gmail.com>
2023-11-29 20:02:28 -0300

    Atualização do SG EC2

--- terraform/aws/modules/rede/main.tf
+++ terraform/aws/modules/rede/main.tf
@@ -69,6 +69,13 @@ resource "aws_security_group" "sg-ec2" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
