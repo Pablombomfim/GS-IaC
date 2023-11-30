c4157355f082fe77256cc05fc79897de94fb373b
Pablombomfim <pablo.mb3107@gmail.com>
2023-11-29 21:46:14 -0300

    Correção IP 3

--- terraform/azure/orquestrador.tf
+++ terraform/azure/orquestrador.tf
@@ -1,159 +1,159 @@
 resource "azurerm_resource_group" "rg" {
-    name     = "RG-Iac-Test"
-    location = "eastus"
+  name     = "RG-Iac-Test"
+  location = "eastus"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-    name                = "staticsite-vm-vnet"
-    location            = "eastus"
-    resource_group_name = "RG-Iac-Test"
-    address_space       = "172.0.0.0/16"
+  name                = "staticsite-vm-vnet"
+  location            = "eastus"
+  resource_group_name = "RG-Iac-Test"
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet-1" {
-    name                 = "staticsite-vm-subnet-1"
-    resource_group_name  = "RG-Iac-Test"
-    virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixes     = ["172.0.0.0/24"]
+  name                 = "staticsite-vm-subnet-1"
+  resource_group_name  = "RG-Iac-Test"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/24"]
 }
 
 resource "azurerm_subnet" "subnet-2" {
-        name                 = "staticsite-vm-subnet-2"
-        resource_group_name  = "RG-Iac-Test"
-        virtual_network_name = azurerm_virtual_network.vnet.name
-        address_prefixes     = ["172.0.1.0/24"]
+  name                 = "staticsite-vm-subnet-2"
+  resource_group_name  = "RG-Iac-Test"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 
 resource "azurerm_public_ip" "public-ip" {
-    name                = "staticsite-vm-public-ip"
-    location            = "eastus"
-    resource_group_name = "RG-Iac-Test"
-    allocation_method   = "Dynamic"
-    domain_name_label   = "staticsite-vm"
+  name                = "staticsite-vm-public-ip"
+  location            = "eastus"
+  resource_group_name = "RG-Iac-Test"
+  allocation_method   = "Dynamic"
+  domain_name_label   = "staticsite-vm"
 }
 
 resource "azurerm_network_security_group" "sgdaazurelb" {
-        name                = "staticsite-vm-nsg"
-        location            = "e"
-        resource_group_name = "RG-Iac-Test"
-        security_rule {
-                name                       = "HTTP-Inbound"
-                priority                   = 1021
-                direction                  = "Inbound"
-                access                     = "Allow"
-                protocol                   = "Tcp"
-                source_port_range          = "*"
-                destination_port_range     = "80"
-                source_address_prefix      = "*"
-                destination_address_prefix = "*"
-        }
-        security_rule {
-                name                       = "Outbound"
-                priority                   = 1023
-                direction                  = "Outbound"
-                access                     = "Allow"
-                protocol                   = "*"
-                source_port_range          = "*"
-                destination_port_range     = "*"
-                source_address_prefix      = "*"
-                destination_address_prefix = "*"
-        }
+  name                = "staticsite-vm-nsg"
+  location            = "e"
+  resource_group_name = "RG-Iac-Test"
+  security_rule {
+    name                       = "HTTP-Inbound"
+    priority                   = 1021
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "Outbound"
+    priority                   = 1023
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_security_group" "sgdasvm" {
-        name                = "staticsite-vm-nsg"
-        location            = "eastus"
-        resource_group_name = "RG-Iac-Test"
-        security_rule {
-                name                       = "HTTP-Inbound"
-                priority                   = 1021
-                direction                  = "Inbound"
-                access                     = "Allow"
-                protocol                   = "Tcp"
-                source_port_range          = "*"
-                destination_port_range     = "80"
-                source_address_prefix      = "*"
-                destination_address_prefix = "*"
-        }
-        security_rule {
-                name                       = "SSH-Inbound"
-                priority                   = 1022
-                direction                  = "Inbound"
-                access                     = "Allow"
-                protocol                   = "Tcp"
-                source_port_range          = "*"
-                destination_port_range     = "22"
-                source_address_prefix      = "*"
-                destination_address_prefix = "*"
-        }
-        security_rule {
-                name                       = "Outbound"
-                priority                   = 1023
-                direction                  = "Outbound"
-                access                     = "Allow"
-                protocol                   = "*"
-                source_port_range          = "*"
-                destination_port_range     = "*"
-                source_address_prefix      = "*"
-                destination_address_prefix = "*"
-        }
+  name                = "staticsite-vm-nsg"
+  location            = "eastus"
+  resource_group_name = "RG-Iac-Test"
+  security_rule {
+    name                       = "HTTP-Inbound"
+    priority                   = 1021
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "SSH-Inbound"
+    priority                   = 1022
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "Outbound"
+    priority                   = 1023
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface" "nic-azurelb" {
-        name                = "staticsite-vm-nic-azurelb"
-        location            = "eastus"
-        resource_group_name = "RG-Iac-Test"
-        ip_configuration {
-                name                          = "staticsite-vm-nic-azurelb-ip"
-                subnet_id                     = azurerm_subnet.subnet-1.id
-                private_ip_address_allocation = "Dynamic"
-                public_ip_address_id          = azurerm_public_ip.public-ip.id
-        }
+  name                = "staticsite-vm-nic-azurelb"
+  location            = "eastus"
+  resource_group_name = "RG-Iac-Test"
+  ip_configuration {
+    name                          = "staticsite-vm-nic-azurelb-ip"
+    subnet_id                     = azurerm_subnet.subnet-1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public-ip.id
+  }
 }
 
 resource "azurerm_network_interface" "nic-azurevm-1" {
-        name                = "staticsite-vm-nic-azurevm-1"
-        location            = "eastus"
-        resource_group_name = "RG-Iac-Test"
-        ip_configuration {
-                name                          = "staticsite-vm-nic-azurevm-ip"
-                subnet_id                     = azurerm_subnet.subnet-1.id
-                private_ip_address_allocation = "Dynamic"
-        }
+  name                = "staticsite-vm-nic-azurevm-1"
+  location            = "eastus"
+  resource_group_name = "RG-Iac-Test"
+  ip_configuration {
+    name                          = "staticsite-vm-nic-azurevm-ip"
+    subnet_id                     = azurerm_subnet.subnet-1.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
 resource "azurerm_network_interface" "nic-azurevm-2" {
-        name                = "staticsite-vm-nic-azurevm-2"
-        location            = "eastus"
-        resource_group_name = "RG-Iac-Test"
-        ip_configuration {
-                name                          = "staticsite-vm-nic-azurevm-ip"
-                subnet_id                     = azurerm_subnet.subnet-1.id
-                private_ip_address_allocation = "Dynamic"
-        }
+  name                = "staticsite-vm-nic-azurevm-2"
+  location            = "eastus"
+  resource_group_name = "RG-Iac-Test"
+  ip_configuration {
+    name                          = "staticsite-vm-nic-azurevm-ip"
+    subnet_id                     = azurerm_subnet.subnet-1.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
 resource "azurerm_network_interface" "nic-azurevm-3" {
-        name                = "staticsite-vm-nic-azurevm-3"
-        location            = "eastus"
-        resource_group_name = "RG-Iac-Test"
-        ip_configuration {
-                name                          = "staticsite-vm-nic-azurevm-ip"
-                subnet_id                     = azurerm_subnet.subnet-2.id
-                private_ip_address_allocation = "Dynamic"
-        }
+  name                = "staticsite-vm-nic-azurevm-3"
+  location            = "eastus"
+  resource_group_name = "RG-Iac-Test"
+  ip_configuration {
+    name                          = "staticsite-vm-nic-azurevm-ip"
+    subnet_id                     = azurerm_subnet.subnet-2.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
 resource "azurerm_network_interface" "nic-azurevm-4" {
-        name                = "staticsite-vm-nic-azurevm-4"
-        location            = "eastus"
-        resource_group_name = "RG-Iac-Test"
-        ip_configuration {
-                name                          = "staticsite-vm-nic-azurevm-ip"
-                subnet_id                     = azurerm_subnet.subnet-2.id
-                private_ip_address_allocation = "Dynamic"
-        }
+  name                = "staticsite-vm-nic-azurevm-4"
+  location            = "eastus"
+  resource_group_name = "RG-Iac-Test"
+  ip_configuration {
+    name                          = "staticsite-vm-nic-azurevm-ip"
+    subnet_id                     = azurerm_subnet.subnet-2.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
 resource "azurerm_virtual_machine" "maquina1" {
@@ -184,7 +184,7 @@ resource "azurerm_virtual_machine" "maquina1" {
     computer_name  = "staticsite-vm-1"
     admin_username = "adminuser"
     admin_password = "AdminPassword123!"
-        custom_data                     = <<-EOF
+    custom_data    = <<-EOF
       #!/bin/bash
       sudo apt-get update
       sudo apt-get install -y apache2
@@ -225,7 +225,7 @@ resource "azurerm_virtual_machine" "maquina2" {
     computer_name  = "staticsite-vm-2"
     admin_username = "adminuser"
     admin_password = "AdminPassword123!"
-        custom_data                     = <<-EOF
+    custom_data    = <<-EOF
       #!/bin/bash
       sudo apt-get update
       sudo apt-get install -y apache2
@@ -266,7 +266,7 @@ resource "azurerm_virtual_machine" "maquina3" {
     computer_name  = "staticsite-vm-3"
     admin_username = "adminuser"
     admin_password = "AdminPassword123!"
-        custom_data                     = <<-EOF
+    custom_data    = <<-EOF
       #!/bin/bash
       sudo apt-get update
       sudo apt-get install -y apache2
@@ -307,7 +307,7 @@ resource "azurerm_virtual_machine" "maquina4" {
     computer_name  = "staticsite-vm-4"
     admin_username = "adminuser"
     admin_password = "AdminPassword123!"
-        custom_data                     = <<-EOF
+    custom_data    = <<-EOF
       #!/bin/bash
       sudo apt-get update
       sudo apt-get install -y apache2
